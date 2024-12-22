2bb56de9d4b83d66dbb2e34418d27fbc64c2ad59
Radim Staněk <gtomyasek@gmail.com>
2024-12-22 17:00:01 +0100

    Add revalidatePath calls after database mutations
    Revalidation ensures that the affected pages reflect the latest data after changes to the database. The redundant revalidation logic in `InsertComponent` was removed to avoid duplication, keeping it solely in the request functions.

--- src/components/InsertComponent.tsx
+++ src/components/InsertComponent.tsx
@@ -3,14 +3,12 @@
 import { insertTestTableRow } from '@/lib/requests';
 import { Button } from 'gtomy-lib';
 import { useCallback } from 'react';
-import { revalidatePath } from 'next/cache';
 
 export function InsertComponent() {
   const onClick = useCallback(() => {
     insertTestTableRow('b' + Math.random())
       .then((response) => console.log(response))
-      .catch((error) => console.error(error))
-      .finally(() => revalidatePath('/'));
+      .catch((error) => console.error(error));
   }, []);
 
   return <Button onClick={onClick}>Insert row</Button>;
--- src/lib/requests.ts
+++ src/lib/requests.ts
@@ -1,6 +1,7 @@
 'use server';
 
 import { pool } from '@/lib/postgress';
+import { revalidatePath } from 'next/cache';
 
 export async function getTestTableData(): Promise<{ data: string }[]> {
   const client = await pool.connect();
@@ -18,6 +19,7 @@ export async function removeTestTableDataRow(data: string): Promise<void> {
     await client.query("DELETE FROM test_table WHERE data = '" + data + "'");
   } finally {
     client.release();
+    revalidatePath('/');
   }
 }
 
@@ -27,5 +29,6 @@ export async function insertTestTableRow(data: string): Promise<void> {
     await client.query("INSERT INTO test_table VALUES ('" + data + "')");
   } finally {
     client.release();
+    revalidatePath('/');
   }
 }
