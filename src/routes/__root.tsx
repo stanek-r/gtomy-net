156d29232c0c672cf4177baa4ebea8d2e8ac7764
Radim Staněk <gtomyasek@gmail.com>
2025-04-05 09:34:43 +0200

    Remove unused Providers component and related context setup.
    The Providers component and its GTomyContext setup were removed as they were no longer in use. This also simplifies the root route component by directly rendering the layout without wrapping it in unnecessary context management.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,19 +2,16 @@ import { Menu } from '@/components/Menu';
 import { createRootRoute, Outlet } from '@tanstack/react-router';
 import { Footer } from '@/components/Footer.tsx';
 import { NotFound } from '@/components/NotFound.tsx';
-import { Providers } from '@/components/Providers.tsx';
 
 export const Route = createRootRoute({
   component: () => (
-    <Providers>
-      <div className="flex flex-col min-h-screen">
-        <Menu />
-        <main className="max-w-7xl mx-auto px-4 pt-8 pb-40 flex flex-col gap-y-16 flex-1">
-          <Outlet />
-        </main>
-        <Footer />
-      </div>
-    </Providers>
+    <div className="flex flex-col min-h-screen">
+      <Menu />
+      <main className="max-w-7xl mx-auto px-4 pt-8 pb-40 flex flex-col gap-y-16 flex-1">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
   ),
   notFoundComponent: () => {
     return <NotFound />;
